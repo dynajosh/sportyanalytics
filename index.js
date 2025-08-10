6bcc5484280474affc726188f69ca2203d25f9c4
dynajosh <oriakhi78@gmail.com>
2025-08-10 07:06:00 +0100

    puppeteer switch to headless

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const axios = require('axios');
-
+const dotenv = require('dotenv');
 const express = require('express');
 const puppeteer = require('puppeteer');
 const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-
+dotenv.config();
 app.use(cors({
 //   origin: 'http://localhost:5173',
 origin: ['http://localhost:5173', 'https://antisporty.netlify.app/'],
@@ -88,22 +88,35 @@ app.post('/login', async (req, res) => {
   try {
     console.log('Attempting to launch browser...');
     
+    // const browser = await puppeteer.launch({
+    //   headless: true,
+    //   args: [
+    //     '--no-sandbox',
+    //     '--disable-setuid-sandbox',
+    //     '--disable-dev-shm-usage',
+    //     '--disable-accelerated-2d-canvas',
+    //     '--no-first-run',
+    //     '--no-zygote',
+    //     '--single-process',
+    //     '--disable-gpu',
+    //     '--disable-background-timer-throttling',
+    //     '--disable-backgrounding-occluded-windows',
+    //     '--disable-renderer-backgrounding'
+    //   ]
+    // });
+
     const browser = await puppeteer.launch({
-      headless: true,
-      args: [
-        '--no-sandbox',
-        '--disable-setuid-sandbox',
-        '--disable-dev-shm-usage',
-        '--disable-accelerated-2d-canvas',
-        '--no-first-run',
-        '--no-zygote',
-        '--single-process',
-        '--disable-gpu',
-        '--disable-background-timer-throttling',
-        '--disable-backgrounding-occluded-windows',
-        '--disable-renderer-backgrounding'
-      ]
-    });
+    args: [
+      "--disable-setuid-sandbox",
+      "--no-sandbox",
+      "--single-process",
+      "--no-zygote",
+    ],
+    executablePath:
+      process.env.NODE_ENV === "production"
+        ? process.env.PUPPETEER_EXECUTABLE_PATH
+        : puppeteer.executablePath(),
+  });
 
     console.log('Browser launched successfully');
 
